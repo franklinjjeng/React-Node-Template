ac2388a167d013e39567b10b7cbf8417092af19e
Franklin Jeng <franklinjjeng@gmail.com>
2018-05-10 18:49:44 -0700

    (cleanup) removing unused db files

--- src/db/schema.js
+++
@@ -1,8 +0,0 @@
-// // Setup relational database with Sequelize ORM
-// var mysql = require('mysql');
-// var Sequelize = require('sequelize');
-// var sequelize = new Sequelize('publishus', 'root', '');
-
-// // Setup document database with Mongoose ORM
-// var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/publishus');
