6c9aad86c9275a6aa10c456e20fba637098120dc
Franklin Jeng <franklinjjeng@gmail.com>
2018-05-24 16:18:42 -0700

    (feat) adding react-router template (#1)

---
+++ src/app/components/NoPage404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NoPage404(props) {
+  return (
+    <div>
+      <span>Did you get lost? There seems to be nothing here.</span>
+    </div>
+  );
+}
